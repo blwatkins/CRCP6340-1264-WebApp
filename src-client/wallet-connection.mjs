c76ff8083133bf6b14d11d7db949fbc8f63917ee
blwatkins <blwatkins@users.noreply.github.com>
2026-05-26 23:50:11 -0500

    Configure AppKit and WagmiAdapter from reown.

---
+++ src-client/wallet-connection.mjs
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026 Brittni Watkins.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { createAppKit } from '@reown/appkit'
+
+import { baseSepolia } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+const projectId = '';
+
+const metadata = {
+    name: 'AppKit',
+    description: 'AppKit Example',
+    url: 'http://localhost:3000',
+    icons: ['https://avatars.githubusercontent.com/u/179229932']
+};
+
+const networks = [baseSepolia];
+
+const wagmiAdapter = new WagmiAdapter({
+    networks,
+    projectId
+})
+
+const modal = createAppKit({
+    adapters: [wagmiAdapter],
+    networks,
+    defaultNetwork: baseSepolia,
+    projectId,
+    metadata,
+});
+
+export function hi() {
+}
