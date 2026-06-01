e4df913635f116bc15380e219566fdfb2a762de7
blwatkins <blwatkins@users.noreply.github.com>
2026-06-01 16:18:27 -0500

    Update vite configuration.

--- vite.config.mjs
+++ vite.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
             output: {
                 entryFileNames: 'bundle.js',
                 chunkFileNames: '[name]-[hash].js',
-                assetFileNames: '[name]-[hash].[ext]'
+                assetFileNames: '[name]-[hash][extname]'
             }
         }
     }
